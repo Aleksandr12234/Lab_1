a85f77e1d0933c1124bf011d1d29db09ddd30d8a
Aleksandr12234 <566566sana@gmail.com>
2025-03-19 21:49:06 +0600

    создание кода

---
+++ Tests.c
@@ -0,0 +1,22 @@
+﻿#include "list.h"
+
+void FillWithTestValues(List *list) {
+  AddToBeginning(list, 10);
+  AddToEnd(list, 20);
+  AddToEnd(list, 30);
+  AddToMiddle(list, 15, 1);
+  AddToBeginning(list, 5);
+  Find(list, 1);
+  Count(list);
+  Delete(list, 1);
+}
+
+int main() {
+  List list;
+
+  Create_List(&list, 10);
+
+  FillWithTestValues(&list);
+
+  return 0;
+}
