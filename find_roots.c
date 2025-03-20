f25ba2b70b175fb080c355290811c170f27455af
Aleksandr12234 <566566sana@gmail.com>
2025-03-20 13:22:49 +0600

    Создание кода

---
+++ find_roots.c
@@ -0,0 +1,40 @@
+﻿
+double* find_roots(double a, double b, double c, int* count) {
+    if (a == 0) {
+        *count = 0;
+        return NULL;
+    }
+
+    double D = b * b - 4 * a * c;
+
+    if (D > 0) {
+        double* roots = (double*)malloc(2 * sizeof(double));
+        if (roots == NULL) {
+            exit(1);
+        }
+        roots[0] = (-b - sqrt(D)) / (2 * a);
+        roots[1] = (-b + sqrt(D)) / (2 * a);
+        if (roots[0] > roots[1]) {
+            double temp = roots[0];
+            roots[0] = roots[1];
+            roots[1] = temp;
+        }
+        *count = 2;
+        return roots;
+    }
+    else if (D == 0) {
+        double* root = (double*)malloc(sizeof(double));
+        if (root == NULL) {
+            exit(1);
+        }
+        root[0] = -b / (2 * a);
+        *count = 1;
+        return root;
+    }
+    else {
+        *count = 0;
+        return NULL;
+    }
+}
+
+
---
+++ roots.h
@@ -0,0 +1,10 @@
+﻿#include <math.h>
+#include <stdlib.h>
+
+double* find_roots(double a, double b, double c, int* count) 
+    ;
+    
+
+
+
+
